5b0f8df87ec95c8ab5d91c1ffcb5e6f016ee39aa
iksaku <iksaku@me.com>
2014-05-28 17:22:38 -0500

    Sessions again... I need help with this... I don't know how to access to it without using "new Sessions()"

---
+++ src/EssentialsPE/API/Sessions.php
@@ -0,0 +1,42 @@
+<?php
+namespace EssentialsPE\API;
+
+use pocketmine\Player;
+
+class Sessions {
+    public $sessions = array();
+    public $default = array();
+
+    public function setDefault($key, $value){
+        //$this->default[$key] = $value;
+        $this->default[$key] = $value;
+    }
+
+    public function create(Player $player){
+        $this->sessions[$player] = $this->default;
+    }
+
+    public function remove($player){
+        if(!isset($this->sessions[$player])){
+            return false;
+        }else{
+            unset($this->sessions[$player]);
+        }
+    }
+
+    public function set(Player $player, $key, $value){
+        if(!isset($this->sessions[$player][$key])){
+            return false;
+        }else{
+            $this->sessions[$player][$key] = $value;
+        }
+    }
+
+    public function get(Player $player, $key){
+        if(!isset($this->sessions[$player][$key])){
+            return false;
+        }else{
+            return $this->sessions[$player][$key];
+        }
+    }
+} 
